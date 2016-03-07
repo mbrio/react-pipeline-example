ccd17aeddbd0ac4192ea2280b5119cc8f8f1fa64
Michael Diolosa <michael.diolosa@gmail.com>
2016-03-07 08:34:27 -0500

    Update to v14.7 of react-pipeline

--- spec/unit/Print.spec.js
+++ spec/unit/Print.spec.js
@@ -1,17 +1,18 @@
-jest.dontMock('../../src/Print.js');
+jest.dontMock('react-pipeline');
 
 import React from 'react';
 
+const ReactPipeline = require('react-pipeline').default;
 const Print = require('../../src/Print.js').default;
 
 describe('Print', () => {
   describe('exec', () => {
-    pit('should print to console.log', () => {
+    pit('should print to stdout', () => {
       const message = 'Here is a message';
       const stdout = { write: jest.genMockFunction() };
-      const print = new Print({ stdout: stdout, message }, {});
 
-      return print.start().then(() => {
+      return ReactPipeline.start(<Print stdout={stdout} message={message} />)
+      .then(() => {
         expect(stdout.write.mock.calls.length).toBe(1);
         expect(stdout.write.mock.calls[0][0]).toBe(`${message}\n`);
       });
--- spec/unit/Wait.spec.js
+++ spec/unit/Wait.spec.js
@@ -1,14 +1,12 @@
-jest.dontMock('../../src/Wait.js');
-
 import React from 'react';
+import ReactPipeline from 'react-pipeline';
 
 const Wait = require('../../src/Wait.js').default;
 
 describe('Wait', () => {
   describe('exec', () => {
     it('should wait for default duration', (done) => {
-      const wait = new Wait({}, {});
-      wait.start().then(() => {
+      ReactPipeline.start(<Wait />).then(() => {
         expect(Wait.defaultDuration).toBeDefined();
         expect(setTimeout.mock.calls.length).toBe(1);
         expect(setTimeout.mock.calls[0][1]).toBe(Wait.defaultDuration);
@@ -20,8 +18,7 @@ describe('Wait', () => {
     });
 
     it('should wait for specified duration', (done) => {
-      const wait = new Wait({ duration: 500 }, {});
-      wait.start().then(() => {
+      ReactPipeline.start(<Wait duration={500} />).then(() => {
         expect(setTimeout.mock.calls.length).toBe(1);
         expect(setTimeout.mock.calls[0][1]).toBe(500);
 
